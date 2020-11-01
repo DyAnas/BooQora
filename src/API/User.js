40391ef6f2fa22ccf50df6358057ee905ff17948
DyAnas <48991282+DyAnas@users.noreply.github.com>
2020-11-01 15:30:18 +0100

    create spinner for login and handle incorrect password and email is not acitve

--- src/API/User.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-
-export async function adduser(value){
-    const response = await axios.post('http://localhost:8080/api/v1/employees', value);
-    return response;
-}
